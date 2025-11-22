8e9a4e4abfee9baa7a9bb5893f177da868de723f
Julinium <121466317+Julinium@users.noreply.github.com>
2025-11-22 12:53:53 +0100

    Update views.py

--- portal/views.py
+++ portal/views.py
@@ -74,7 +74,7 @@ def cons_list(request):
 	qd = {}
 	ct = mn = mx = o = r = c = l = None #, None, None, None, None, None
 	cons = Consultation.objects.filter(active = True, cancelled = False)
-	cons = cons.filter(date_limite_depot__gte = datetime.now() - timedelta(hours=C.PAST_GRACEFUL_HOURS))
+	# cons = cons.filter(date_limite_depot__gte = datetime.now() - timedelta(hours=C.PAST_GRACEFUL_HOURS))
 	if 'pr' in request.GET and request.GET['pr']:
 		pr = request.GET['pr']
 		if pr != '' : cons = cons.filter(procedure_annonce__id = str(pr))
